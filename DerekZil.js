769942ecfccea96cb18a6660f8b7f14a6e004ca2
Derek Zil <DerAndZil@Gmail.com>
2013-05-20 13:12:41 -0300

    Update DerekZil.js

--- DerekZil.js
+++ DerekZil.js
@@ -442,7 +442,7 @@ function overPlayedSongs(data) {
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
 	}
 	if (Models.room.data.media.duration > 481) {
-		API.sendChat("/me auto skip activated! song exceeds 8 minutes long");
+		API.sendChat("/me auto skip activated! Song exceeds 8 minutes long.");
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],true,true,1,5);", 250);
 		setTimeout("new ModerationForceSkipService;", 500);
 		setTimeout("new RoomPropsService(document.location.href.split('/')[3],false,true,1,5);", 750);
